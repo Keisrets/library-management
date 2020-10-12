bb6a5b31e47c66820d73be371ead6ff60f33c59d
Kristers Nikitins <kristers.nikitins@inbox.lv>
2020-10-12 10:56:36 +0300

    Initial project commit

---
+++ src/test/java/kn18012/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kn18012.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
