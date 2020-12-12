38ab01e6c5ffe02796cd55bb39556cd9a4b2bebd
Kristers Nikitins <kristers.nikitins@inbox.lv>
2020-12-12 22:50:24 +0200

    Changed urls accessible by librarians

--- src/main/java/kn18012/librarymanagement/config/SecurityConfig.java
+++ src/main/java/kn18012/librarymanagement/config/SecurityConfig.java
@@ -1,12 +1,8 @@
 package kn18012.librarymanagement.config;
 
-import kn18012.librarymanagement.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,15 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
-//        http.authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/login", "/register").permitAll()
-//                .antMatchers("/lib-admin/**").access("hasAuthority('admin')").anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login").successForwardUrl("/").defaultSuccessUrl("/succ").failureUrl("/err").permitAll()
-//                .and().logout().logoutSuccessUrl("/").permitAll().logoutUrl("/logout")
-//                .and().headers().frameOptions().disable()
-//                .and().csrf().disable();
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers("/lib-admin/**").access("hasAuthority('admin')").anyRequest().authenticated()
+                .antMatchers("/lib-dashboard/**").access("hasAuthority('librarian')").anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login-failed").permitAll()
+                .and().logout().logoutSuccessUrl("/").permitAll().logoutUrl("/logout").invalidateHttpSession(true)
+                .and().headers().frameOptions().disable()
+                .and().csrf().disable();
     }
 
     @Bean
