c77de4b9f4fe69a8c2ec01fc57b1a38e4d5f8f2b
Kristers Nikitins <kristers.nikitins@inbox.lv>
2020-12-16 20:55:43 +0200

    Refactoring due to LibrarianService name change

--- src/main/java/kn18012/librarymanagement/controller/LibrarianController.java
+++ src/main/java/kn18012/librarymanagement/controller/LibrarianController.java
@@ -2,7 +2,7 @@ package kn18012.librarymanagement.controller;
 
 import kn18012.librarymanagement.service.AuthorService;
 import kn18012.librarymanagement.service.BookService;
-import kn18012.librarymanagement.service.LibrarianService;
+import kn18012.librarymanagement.service.LoanService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/lib-dashboard")
 public class LibrarianController {
 
-    private LibrarianService librarianService;
+    private LoanService loanService;
     private BookService bookService;
     private AuthorService authorService;
 
-    public LibrarianController(LibrarianService librarianService, BookService bookService, AuthorService authorService) {
-        this.librarianService = librarianService;
+    public LibrarianController(LoanService loanService, BookService bookService, AuthorService authorService) {
+        this.loanService = loanService;
         this.bookService = bookService;
         this.authorService = authorService;
     }
@@ -25,7 +25,7 @@ public class LibrarianController {
     public String home(Model model) {
         model.addAttribute("books", bookService.findAll());
         model.addAttribute("authors", authorService.findAllAuthors());
-        model.addAttribute("loans", librarianService.findAllLoans());
+        model.addAttribute("loans", loanService.findAllLoans());
         return "lib/index";
     }
 
