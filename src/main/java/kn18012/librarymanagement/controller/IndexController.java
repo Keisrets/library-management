a9b627996f8b9321960897301dcbcf1bc89667cc
Kristers Nikitins <kristers.nikitins@inbox.lv>
2020-12-19 11:13:08 +0200

    Removing unnecessary code

--- src/main/java/kn18012/librarymanagement/controller/IndexController.java
+++ src/main/java/kn18012/librarymanagement/controller/IndexController.java
@@ -1,10 +1,7 @@
 package kn18012.librarymanagement.controller;
 
 import kn18012.librarymanagement.domain.User;
-import kn18012.librarymanagement.repository.LoanRepository;
 import kn18012.librarymanagement.service.LoanService;
-import kn18012.librarymanagement.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,21 +12,18 @@ public class IndexController {
 
     private LoanService loanService;
 
-    @Autowired
-    LoanRepository loanRepository;
-
     public IndexController(LoanService loanService) {
         this.loanService = loanService;
     }
 
     @GetMapping("/")
     public String index() {
-        return ("index");
+        return "index";
     }
 
     @GetMapping("/my-loans")
     public String showLoanView(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("loans", loanRepository.findLoansByUser(user));
+        model.addAttribute("loans", loanService.findLoansByUser(user));
         return "usr/loans";
     }
 }
