c799a0e685ec52cdd57551c8412f12820b0f0800
Kristers Nikitins <kristers.nikitins@inbox.lv>
2021-01-01 20:10:07 +0200

    Modified failed login url

--- src/main/java/kn18012/librarymanagement/config/SecurityConfig.java
+++ src/main/java/kn18012/librarymanagement/config/SecurityConfig.java
@@ -27,8 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .and().authorizeRequests().antMatchers("/login", "/register").permitAll()
                 .and().authorizeRequests().antMatchers("/lib-admin/**").access("hasAuthority('admin')")
-                .and().authorizeRequests().antMatchers("/lib-dashboard/**").access("hasAnyAuthority('librarian', 'admin')").anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login-failed").successHandler(authenticationSuccessHandler).permitAll()
+                .and().authorizeRequests().antMatchers("/lib-dashboard/**").access("hasAuthority('librarian')").anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login?failed").successHandler(authenticationSuccessHandler).permitAll()
                 .and().logout().logoutSuccessUrl("/").permitAll().logoutUrl("/logout").invalidateHttpSession(true)
                 .and().headers().frameOptions().disable()
                 .and().csrf().disable();
